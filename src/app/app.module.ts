96dfbad6a76cff16930cb2685c2d9f2a082309ec
Ravitejac6 <ravitejapenugonda.3@gmail.com>
2021-02-06 20:42:12 +0530

    UI added for SignUp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatToolbarModule } from '@angular/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
@@ -30,7 +30,8 @@ import { LoginComponent } from './components/login/login.component';
     MatButtonModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    HttpClientModule  
+    HttpClientModule  ,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
